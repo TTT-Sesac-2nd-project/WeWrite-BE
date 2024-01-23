dacd049bdd24ade5427f5d948ec0fe781262d5b0
SoMin <thals0107@naver.com>
2024-01-23 12:05:52 +0900

    #19 feat: 마이페이지 기능 구현

---
+++ wewrite/src/main/java/site/hyundai/wewrite/domain/auth/service/UserService.java
@@ -0,0 +1,43 @@
+package site.hyundai.wewrite.domain.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.hyundai.wewrite.domain.auth.dto.response.UserResponseDTO;
+import site.hyundai.wewrite.domain.auth.repository.UserRepository;
+import site.hyundai.wewrite.domain.entity.User;
+import site.hyundai.wewrite.domain.group.dto.response.GroupResponseDTO;
+import site.hyundai.wewrite.domain.group.service.GroupService;
+import site.hyundai.wewrite.global.dto.ResponseSuccessDTO;
+import site.hyundai.wewrite.global.util.ResponseUtil;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final ResponseUtil responseUtil;
+    private final GroupService groupService;
+    private final UserRepository userRepository;
+
+    // 유저 정보 조회
+    @Transactional(readOnly = true)
+    public ResponseSuccessDTO<UserResponseDTO> getUserInfo(User user) {
+        // 내 그룹 갯수 조회
+        ResponseSuccessDTO<List<GroupResponseDTO>> myGroupsResponse = groupService.getMyGroups(user);
+        List<GroupResponseDTO> myGroups = myGroupsResponse.getData();
+        int numberOfMyGroups = myGroups != null ? myGroups.size() : 0;
+
+        UserResponseDTO userResponseDTO = new UserResponseDTO(user, numberOfMyGroups);
+        return responseUtil.successResponse(userResponseDTO, HttpStatus.OK);
+    }
+
+    // 유저 이름 변경
+    @Transactional
+    public ResponseSuccessDTO<String> updateUserName(String userName, User user) {
+        user.setUserName(userName);
+        userRepository.save(user);
+        return responseUtil.successResponse("유저 이름 변경 완료", HttpStatus.OK);
+    }
+}
