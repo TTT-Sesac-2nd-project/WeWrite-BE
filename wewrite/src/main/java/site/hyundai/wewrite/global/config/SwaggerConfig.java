24046962da84f836e3baf410c977d19d04b0740b
smartpodo@kakao.com <smartpodo@kakao.com>
2024-01-24 12:31:01 +0900

    fix: swagger Authorize button

--- wewrite/src/main/java/site/hyundai/wewrite/global/config/SwaggerConfig.java
+++ wewrite/src/main/java/site/hyundai/wewrite/global/config/SwaggerConfig.java
@@ -4,32 +4,44 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
+
+
     @Bean
     public Docket api() {
+        // List<Parameter> parameterList = new ArrayList<>();
+        // parameterList.add(apiKey()); // Add the apiKey as a global parameter
 
-
-        List<Parameter> parameterList = new ArrayList<>();
         return new Docket(DocumentationType.SWAGGER_2)
-//                .consumes(getConsumeContentTypes())
+                //                .consumes(getConsumeContentTypes())
 //                .produces(getProduceContentTypes())
-                .select().apis(RequestHandlerSelectors.any())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
+
     }
 
 //    private Set<String> getConsumeContentTypes() {
@@ -47,7 +59,34 @@ public class SwaggerConfig implements WebMvcConfigurer {
 //    }
 
     private ApiKey apiKey() {
-        return new ApiKey("Bearer +accessToken", "Authorization", "header");
+        return new ApiKey("JWT", "token", "header");
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Wewrite API 문서")
+                .description("Wewrite API 문서입니다.")
+                .version("1.0.1")
+                .licenseUrl("https://github.com/TTT-Sesac-2nd-project")
+                .build();
+    }
+
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
     }
 
     @Override
